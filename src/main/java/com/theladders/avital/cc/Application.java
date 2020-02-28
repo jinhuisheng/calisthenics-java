ec2b0b5c0456e2a3d1dba4cee25b9177014153b3
金慧生 <jinhuisheng@wowoshijie.com>
2020-02-28 12:26:26 +0800

    rename employerJobs_temp to employerJobs

--- src/main/java/com/theladders/avital/cc/Application.java
+++ src/main/java/com/theladders/avital/cc/Application.java
@@ -12,7 +12,7 @@ public class Application {
     private HashMap<String, List<JobApplication>> jobSeekerApplications = new HashMap<>();
     private List<JobApplication> failedApplications = new ArrayList<>();
     private HashMap<String, List<Job>> seekerConcernJobs = new HashMap<>();
-    private EmployerJobs employerJobs_temp = new EmployerJobs();
+    private EmployerJobs employerJobs = new EmployerJobs();
 
 
     public void applyJob(String employerName, String jobName, String jobSeekerName, String resumeApplicantName, LocalDate applicationTime, JobType jobType) throws NotSupportedJobTypeException, RequiresResumeForJReqJobException, InvalidResumeException {
@@ -22,7 +22,7 @@ public class Application {
 
     void publishJob(String employerName, String jobName, JobType jobType) throws NotSupportedJobTypeException {
         checkJobTypeWhenPublish(jobType);
-        employerJobs_temp.addJob(employerName, jobName, jobType.getName());
+        employerJobs.addJob(employerName, jobName, jobType.getName());
     }
 
     private void checkJobTypeWhenApplyCommand(String employerName, String jobName, String jobSeekerName, String resumeApplicantName, LocalDate applicationTime, JobType jobType) throws RequiresResumeForJReqJobException, InvalidResumeException {
@@ -61,7 +61,7 @@ public class Application {
 
 
     List<Job> getEmployerJobs(String employerName) {
-        return employerJobs_temp.getEmployerJobs(employerName);
+        return employerJobs.getEmployerJobs(employerName);
     }
 
     List<JobApplication> getJobSeekerApplications(String employerName) {
