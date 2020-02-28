5516158ad6735bf29a98244fa7bcb0a357eb1f68
金慧生 <jinhuisheng@wowoshijie.com>
2020-02-28 12:35:35 +0800

    create class FailedApplications，并将对failedApplications操作的方法复制到FailedFailedApplications class中

---
+++ src/main/java/com/theladders/avital/cc/FailedApplications.java
@@ -0,0 +1,24 @@
+package com.theladders.avital.cc;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author huisheng.jin
+ * @date 2020/2/28.
+ */
+public class FailedApplications {
+    private List<JobApplication> failedApplications = new ArrayList<>();
+
+    private void addFailedApplications(String employerName, String jobName, String jobType, LocalDate applicationTime) {
+        failedApplications.add(new JobApplication(jobName, jobType, applicationTime, employerName));
+    }
+
+    public int getUnsuccessfulApplications(String employerName, String jobName) {
+        return (int) failedApplications.stream()
+                .filter(job -> job.getJobName().equals(jobName) && job.getEmployerName().equals(employerName))
+                .count();
+    }
+
+}
